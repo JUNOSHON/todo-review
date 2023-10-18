f099834a510aca3ae39f9e4a791bbfe46d5ac6cc
junoshon <sanoldfox@naver.com>
2023-10-18 21:01:54 +0900

    feat: 공유되는 상태 filter 추가, props로 필터 목록 배열 전달

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 import TodoList from "./components/TodoList/TodoList.jsx";
+import Header from "./components/Header/Header.jsx";
+
+
+const filters = ["모두", "미완료됨", "완료됨"];
 
 function App() {
+  const [filter, setFilter] = useState(filters[0]);
   
-
   return (
     <>
-      <TodoList/>
+      <Header filters={filters} filter={filter} onFilterChange={setFilter}/>
+      <TodoList filter={filter}/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
