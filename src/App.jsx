8614e3e006cc9bedecd267ae1312663fcce28814
junoshon <sanoldfox@naver.com>
2023-10-18 20:26:43 +0900

    feat: TodoList 추가, 보일러 플레이트 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import TodoList from "./components/TodoList/TodoList.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-    
+      <TodoList/>
     </>
   )
 }
